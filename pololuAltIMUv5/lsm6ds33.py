b0a750e3eb7b55b084d691df7a143aaaba6602b6
workordeath <alfonso.oramas@icloud.com>
2025-04-05 21:09:33 -0400

    NEW: Mutliplied the sensitity value against the accelerometer for an accurate measurement in the 2g range.

--- pololuAltIMUv5/lsm6ds33.py
+++ pololuAltIMUv5/lsm6ds33.py
@@ -9,6 +9,7 @@ class LSM6DS33:
         self.CTRL2_G = 0x11  # Gyroscope control register
         self.OUTX_L_G = 0x22  # Gyroscope output registers
         self.OUTX_L_XL = 0x28  # Accelerometer output registers
+        self.ACCELEROMETER_SENSITIVITY = 0.000061
         self.GYROSCOPE_SENSITIVITY = 8.75 / 1000
         self.initialize()
 
@@ -43,11 +44,18 @@ class LSM6DS33:
         return gravity_x_axis, gravity_y_axis, gravity_z_axis
 
     def read_raw_accelerometer(self):
-        ax_l, ax_h, ay_l, ay_h, az_l, az_h = self.i2c.read_i2c_block_data(self.OUTX_L_XL, 6)
-        ax = struct.unpack('<h', bytes([ax_l, ax_h]))[0]
-        ay = struct.unpack('<h', bytes([ay_l, ay_h]))[0]
-        az = struct.unpack('<h', bytes([az_l, az_h]))[0]
-        return ax, ay, az
+        accelerometer_x_axis_low, accelerometer_x_axis_high, accelerometer_y_axis_low, accelerometer_y_axis_high, accelerometer_z_axis_low, accelerometer_z_axis_high = self.i2c.read_i2c_block_data(self.OUTX_L_XL, 6)
+        accelerometer_x_axis = struct.unpack('<h', bytes([accelerometer_x_axis_low, accelerometer_x_axis_high]))[0]
+        accelerometer_y_axis = struct.unpack('<h', bytes([accelerometer_y_axis_low, accelerometer_y_axis_high]))[0]
+        accelerometer_z_axis = struct.unpack('<h', bytes([accelerometer_z_axis_low, accelerometer_z_axis_high]))[0]
+        return accelerometer_x_axis, accelerometer_y_axis, accelerometer_z_axis
+    
+    def read_accelerometer(self):
+        accelerometer_x_axis, accelerometer_y_axis, accelerometer_z_axis = self.read_raw_accelerometer()
+        accelerometer_x_axis *= self.ACCELEROMETER_SENSITIVITY
+        accelerometer_y_axis *= self.ACCELEROMETER_SENSITIVITY
+        accelerometer_z_axis *= self.ACCELEROMETER_SENSITIVITY
+        return accelerometer_x_axis, accelerometer_y_axis, accelerometer_z_axis
 
     def isolate_gravity(self, accelerometer_reading, CONSTANT_SMOOTHING, current_gravity):
         self.current_gravity = CONSTANT_SMOOTHING * current_gravity + (1 - CONSTANT_SMOOTHING) * accelerometer_reading
